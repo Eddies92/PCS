c7adc31ca068e3d6dac64e6a6ac1162d7b4a77a1
Eddie Schmool <eschmool@icloud.com>
2019-04-02 21:02:59 -0400

    '38'

---
+++ python/37/homework1.py
@@ -0,0 +1,7 @@
+name = 'Eddie Schmool'
+address = '10 Sharon Ct'
+siblings = ['Suzie', 'Adele', 'Albert', 'Rachel', 'Yehuda', 'Rivka', 'Moshe']
+print(f'Name: {name} \n Adress: {address} \n Siblings: {siblings}')
+print(name[::3])
+second_to_last = siblings[-2]
+print(second_to_last[1:-1:])
