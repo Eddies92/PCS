48c00716bb0aa8bcfaf58455b5ef77e0ade99fe3
Eddie Schmool <eschm@Isaac-NJ.saramax.com>
2019-06-26 09:14:18 -0400

    '55'

---
+++ javascript/55/hw.js
@@ -0,0 +1,46 @@
+const weekUtils = (function () {
+    'use strict';
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturay'];
+
+    return {
+        getDay: function (index) {
+            return days[index - 1];
+        },
+
+        getDayNum: function (name) {
+            for (let i = 0; i < days.index; i++) {
+                if (days[i] === name) {
+                    return i + 1;
+                }
+            }
+
+            return 'Please insert a Valid Day Name';
+        }
+    };
+}());
+console.log(weekUtils.getDay(5));
+console.log(weekUtils.getDayNum('Thursday'));
+
+const calculator = (function () {
+    'use strict';
+    let rate;
+    let years;
+    return {
+        calcInterest: function (amount) {
+            return amount * rate * years;
+        },
+
+        setRate: function (rate) {
+            rate = this.rate;
+        },
+
+        setYears: function (years) {
+            years = this.years;
+        }
+    };
+}());
+
+console.log('calculator.setYears(30)', calculator.setYears(30));
+console.log('calculator.setRate(0.0395)', calculator.setRate(0.0395));
+console.log('calculator.calcInterest(500000)', calculator.calcInterest(500000));
