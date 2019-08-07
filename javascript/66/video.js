e0a3201c907b71218b368b6ede7e617c46339bc1
Eddie Schmool <eschm@Isaac-NJ.saramax.com>
2019-08-07 07:56:33 -0400

    '66b'

--- javascript/66/video.js
+++ javascript/66/video.js
@@ -7,17 +7,36 @@
 
     function addVideo(newVideo) {
         $('#videos').append(`<div id="video${numOfVids++}"> <video src="${newVideo.linkUrl}" poster="${newVideo.photo}"></video><p>${newVideo.title}</p></div>`);
+    }
+
+    function playVid() {
         $('#video0').click(function () {
-            $('#video0 video')[0].play();
+            if ($('#video0 video')[0].paused) {
+                $('#video0 video')[0].play();
+            } else {
+                $('#video0 video')[0].pause();
+            }
         });
         $('#video1').click(function () {
-            $('#video1 video')[0].play();
+            if ($('#video1 video')[0].paused) {
+                $('#video1 video')[0].play();
+            } else {
+                $('#video1 video')[0].pause();
+            }
         });
         $('#video2').click(function () {
-            $('#video2 video')[0].play();
+            if ($('#video2 video')[0].paused) {
+                $('#video2 video')[0].play();
+            } else {
+                $('#video2 video')[0].pause();
+            }
         });
         $('#video3').click(function () {
-            $('#video3 video')[0].play();
+            if ($('#video3 video')[0].paused) {
+                $('#video3 video')[0].play();
+            } else {
+                $('#video3 video')[0].pause();
+            }
         });
     }
 
@@ -31,8 +50,8 @@
         })
         .then(loadedVideos => {
             loadedVideos.forEach(addVideo);
-            console.log(loadedVideos);
         })
+        .then(playVid)
         .catch(err => console.error(err));
 
 }());
